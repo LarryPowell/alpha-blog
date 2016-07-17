d1787618a80e419266938341636cf5441e7e93cd
Larry Powell <LarryPowell82@gmail.com>
2016-07-17 16:52:45 +0200

    added secure password

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160717115727) do
+ActiveRecord::Schema.define(version: 20160717144839) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20160717115727) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end

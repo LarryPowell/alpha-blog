1319b16c8b1617eeb7d0b7d316449adf921082c6
Larry Powell <LarryPowell82@gmail.com>
2016-07-17 15:02:11 +0200

    added ass between user and articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,11 @@
 class Article < ActiveRecord::Base
-  validates :title, presence: true, length: {minimun: 3, maximum: 50}
-  validates :description, presence: true, length: {minimun: 10, maximum: 300}
-
+  belongs_to :user
+  validates :title,
+            presence: true,
+            length: {minimun: 3, maximum: 50}
+  validates :description,
+            presence: true,
+            length: {minimun: 10, maximum: 300}
+  validates :user_id,
+            presence: true
 end

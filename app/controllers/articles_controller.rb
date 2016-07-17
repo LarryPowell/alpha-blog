107a843cda2278c8c5d2b40d12f8859989c799b6
Larry Powell <LarryPowell82@gmail.com>
2016-07-17 10:26:04 +0200

    added error and flash styling

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,7 +18,7 @@ class ArticlesController < ApplicationController
     #render plain: params[:article].inspect     # for testing purpose
     @article = Article.new(article_params)
     if @article.save
-      flash[:notice] = "Article was successfully created"
+      flash[:success] = "Article was successfully created"
       redirect_to article_path(@article)
     else
       render 'new'
@@ -29,7 +29,7 @@ class ArticlesController < ApplicationController
     #@article = Article.find(params[:id])  ---> moved to method set_article
 
     if @article.update(article_params)
-      flash[:notice] = "Article was updated"
+      flash[:success] = "Article was updated"
       redirect_to article_path(@article)
     else
       render 'edit'
@@ -44,7 +44,7 @@ class ArticlesController < ApplicationController
   def destroy
     #@article = Article.find(params[:id])  ---> moved to method set_article
     @article.destroy
-    flash[:notice] = "Article was deleted sucessfully"
+    flash[:success] = "Article was deleted sucessfully"
     redirect_to article_path
   end
 
